f324ff504a507b7a7515b64d20cbe6bc8fa17536
Karol-zoned <kargul.karolina47@gmail.com>
2026-06-18 00:37:25 +0200

    schema

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## "What drives customer satisfaction on Olist marketplace?"
 
-1. Firstly I created a database from csv files using MS SQL Server - simply imported flat files to the database
+### Firstly I created a database from csv files using MS SQL Server - simply imported flat files to the database
 
 CREATE DATABASE Olist
  - I check the structure of the tables. As some of them couldn't have datatypes assigned during creation, I proceed to assign primary keys and datatypes in the tables by hand. While assigning PKs I discovered
@@ -8,3 +8,7 @@ CREATE DATABASE Olist
 
 INSERT SOME QUERIES
 TO DO: write about relations between tables, add some more about the anomaly with reviews
+
+### Creating relational database model
+
+![Database schema](schema.png)
--- sql queries/ForeignKeys.sql
+++ sql queries/ForeignKeys.sql
@@ -1,2 +1,43 @@
 USE Olist;
 
+--assigning foreign keys
+
+SELECT TOP 10 * FROM dbo.olist_customers;
+SELECT TOP 10 * FROM dbo.olist_order_items;
+SELECT TOP 10 * FROM dbo.olist_order_payments;
+SELECT TOP 10 * FROM dbo.olist_order_reviews;
+SELECT TOP 10 * FROM dbo.olist_orders;
+SELECT TOP 10 * FROM dbo.olist_products;
+SELECT TOP 10 * FROM dbo.olist_sellers;
+SELECT top 10 * from dbo.product_category_name_translation;
+
+ALTER TABLE dbo.olist_orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id)
+REFERENCES dbo.olist_customers (customer_id);
+
+ALTER TABLE dbo.olist_order_items
+ADD CONSTRAINT fk_order_items_orders
+FOREIGN KEY (order_id)
+REFERENCES dbo.olist_orders (order_id);
+
+ALTER TABLE dbo.olist_order_items
+ADD CONSTRAINT fk_order_items_products
+FOREIGN KEY (product_id)
+REFERENCES dbo.olist_products (product_id);
+
+ALTER TABLE dbo.olist_order_items
+ADD CONSTRAINT fk_order_items_sellers
+FOREIGN KEY (seller_id)
+REFERENCES dbo.olist_sellers (seller_id);
+
+ALTER TABLE dbo.olist_order_payments
+ADD CONSTRAINT fk_payments_orders
+FOREIGN KEY (order_id)
+REFERENCES dbo.olist_orders (order_id);
+
+ALTER TABLE dbo.olist_order_reviews
+ADD CONSTRAINT fk_reviews_orders
+FOREIGN KEY (order_id)
+REFERENCES dbo.olist_orders (order_id);
+
